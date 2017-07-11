44395c5621953fb3b2062d9f1ffb9cb8e33f461e
Tony Tan <tanxiucai1@gmail.com>
2017-07-11 20:45:15 +0800

    add type.js to manage const

---
+++ src/store/types.js
@@ -0,0 +1,11 @@
+// can use this file to manage all global 
+//including getters, muataions and actions
+// Vuex2.1 later, it support auto-namespacing
+// ref-link: https://github.com/vuejs/vuex/releases/tag/v2.1.0
+
+//getters
+export const DOUBLE_COUNTER = 'counter/DOUBLE_COUNTER'
+
+// mutations
+
+// actions
